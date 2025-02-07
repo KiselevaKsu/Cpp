d002ad9fd18dec1a975e58241c9a04e81650474b
ksenia <kiselevaksusha14@gmail.com>
2025-02-07 17:28:14 +0300

    start

---
+++ Header.hpp
@@ -0,0 +1,8 @@
+#pragma once
+#ifndef MY_HEADER_HPP
+#define MY_HEADER_HPP
+
+void inputABC(double& a, double& b, double& c);
+double discriminant(double a, double b, double c);
+
+#endif
---
+++ Task1Cpp.cpp
@@ -0,0 +1,29 @@
+﻿#include <iostream>
+#include <cmath>
+
+void inputABC(double& a, double& b, double& c) {
+    using namespace std;
+    cout << "Enter the coefficients a, b, c of the equation ax^2 + bx + c = 0" << endl;
+    cin >> a >> b >> c;
+}
+
+double discriminant(double a, double b, double c) {
+    return b * b - 4 * a * c;
+}
+
+
+int main()
+{
+    double a, b, c;
+    inputABC(a, b, c);
+
+    if (a == 0) {
+        double x = -c / b;
+        using namespace std;
+        cout << "The equation with the given coefficients is not an equation of the form ax^2 + bx + c = 0. X = " << x << endl;
+    }
+
+    double disc = discriminant(a, b, c);
+
+}
+
