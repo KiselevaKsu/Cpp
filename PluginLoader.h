62b904596beccc2e396ccf6245c30abcd0135d8f
ksenia <kiselevaksusha14@gmail.com>
2025-10-16 13:25:42 +0300

    Part2

--- PluginLoader.h
+++ PluginLoader.h
@@ -1,1 +1,22 @@
 #pragma once
+#include "FunctionInterface.h"
+#include <string>
+#include <vector>
+#include <memory>
+#include <windows.h>
+
+struct PluginDesc {
+    std::string path;   // путь до DLL
+    HMODULE module = nullptr;
+    IFunction* funcPtr = nullptr; // указатель на созданную функцию
+};
+
+// для загрузки всех плагинов
+class PluginLoader {
+public:
+    PluginLoader() = default;
+    ~PluginLoader();
+
+    std::vector<std::unique_ptr<PluginDesc>> loadAll(const std::string& folder = "./plugins");
+
+};
