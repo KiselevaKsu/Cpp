057c690c49010d9f0cc699343818dc630bdb0d21
ksenia <kiselevaksusha14@gmail.com>
2025-11-20 16:55:46 +0300

    добавлен конструктор wrapper и хранение указателей на объект и метод

--- wrapper.cpp
+++ wrapper.cpp
@@ -1,4 +1,14 @@
 #include "wrapper.h"
+#include "subject.h"
+
+Wrapper::Wrapper(Subject* obj,
+    int (Subject::* method)(int, int),
+    const std::vector<std::pair<std::string, int>>& defaults)
+{
+    objectRef = obj;
+    methodRef = method;
+    defaultArgs = defaults;
+}
 
 // установка имени, чтобы engine мог по нему искать
 void Wrapper::setName(const std::string& text)
@@ -9,4 +19,9 @@ void Wrapper::setName(const std::string& text)
 const std::string& Wrapper::getName() const
 {
     return name;
+}
+
+int Wrapper::call(const std::vector<std::pair<std::string, int>>& args)
+{
+    return 0;
 }
--- wrapper.h
+++ wrapper.h
@@ -3,16 +3,27 @@
 #include <string>
 #include <vector>
 #include <utility>
+#include <functional>
+
+class Subject;
 
 class Wrapper
 {
 public:
-
-    Wrapper() = default;
+    Wrapper(Subject* obj,
+        int (Subject::* method)(int, int),
+        const std::vector<std::pair<std::string, int>>& defaults);
 
     void setName(const std::string& text);
     const std::string& getName() const;
 
+    int call(const std::vector<std::pair<std::string, int>>& args);
+
 private:
     std::string name;
+
+    Subject* objectRef;
+    int (Subject::* methodRef)(int, int);
+
+    std::vector<std::pair<std::string, int>> defaultArgs;
 };
